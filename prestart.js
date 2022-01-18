575a51f9122815afe28ec8f5c967d00e33456033
EL2020 <el2020202020@gmail.com>
2022-01-18 17:17:19 -0500

    added menu register

--- prestart.js
+++ prestart.js
@@ -1,8 +1,4 @@
-sc.ModUtils = ig.Class.extend({
-    init() {
-        ig.vars.registerVarAccessor("mod", this);
-    },
-
+sc.modUtils = {
     onVarAccess(_a, b) {
         if(b[0] === "mod") {
             switch(b[2]) {
@@ -14,7 +10,32 @@ sc.ModUtils = ig.Class.extend({
             }
         }
     },
-})
 
+    /*
+     * Handles all parts in registering a new submenu, for example, shops.
+     * 
+     * menuName: the name of the submenu entry, accessible by sc.MENU_SUBMENU.{menuName}
+     * menuClass: the class of the actual menu, such as sc.ShopMenu
+     * langName: the name lang entry in assets/data/lang/sc/gui.[lang].json > labels > menu > menu-titles
+     * altName: alternative lang entry, in case that is necessary (vanilla example: save/load uses the same menu, and "load" is used as the alt name.)
+    */
+    registerMenu(menuName, menuClass, langName, altName = null) {
+        if (menuName in sc.MENU_SUBMENU) {
+            ig.warn(`Warning: Submenu with id ${menuName} already taken!`);
+            return false;
+        }
 
-ig.addGameAddon(() => (sc.modUtils = new sc.ModUtils));
+        sc.MENU_SUBMENU[menuName] = menuName;
+
+        sc.SUB_MENU_INFO[sc.MENU_SUBMENU[menuName]] = {
+            Clazz: menuClass,
+            name: langName
+        }
+
+        if(altName) sc.SUB_MENU_INFO[sc.MENU_SUBMENU[menuName]].alt = altName;
+
+        return true;
+    }
+}
+
+ig.vars.registerVarAccessor("mod", sc.modUtils);
